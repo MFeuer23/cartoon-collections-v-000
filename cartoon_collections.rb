90cd7d79647c28b9564709dabcd5ac7cf1ed0e2e
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-08 21:44:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(dwarf_names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map!
+  planeteer_calls.map! do |name|
+    
 end
 
 def long_planeteer_calls# code an argument here
