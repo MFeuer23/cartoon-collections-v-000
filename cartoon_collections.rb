97a1223669211c2630bec5d38a5ac5b59a7d658f
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-08 21:46:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,8 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map! do |name|
-    name.capitalize
+    name.capitalize + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
